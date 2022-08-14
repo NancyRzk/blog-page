ca8a63827d0d8779706a310337c19c7d915cb71d
NancyRzk <96876853+NancyRzk@users.noreply.github.com>
2022-08-14 22:48:03 +0300

    Update Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,40 @@
 import "./login.css";
-
-export default function Login() {
+import React, { useState } from 'react';
+async function loginUser(credentials) {
+ return fetch('http://localhost:8080/login', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json'
+   },
+   body: JSON.stringify(credentials)
+ })
+   .then(data => data.json())
+}
+export default function Login({ setToken }) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      email,
+      password
+    });
+    setToken(token);
+  }
   return (
     <div className="login">
       <span className="loginTitle">Login</span>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={handleSubmit}>
         <label>Email</label>
-        <input className="loginInput" type="text" placeholder="Enter your email..." />
+        <input className="loginInput" type="text" onChange={e => setEmail(e.target.value)} placeholder="Enter your email..." />
         <label>Password</label>
-        <input className="loginInput" type="password" placeholder="Enter your password..." />
+        <input className="loginInput" type="password" onChange={e => setPassword(e.target.value)} placeholder="Enter your password..." />
         <button className="loginButton">Login</button>
       </form>
         <button className="loginRegisterButton">Register</button>
     </div>
   );
 }
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+};
